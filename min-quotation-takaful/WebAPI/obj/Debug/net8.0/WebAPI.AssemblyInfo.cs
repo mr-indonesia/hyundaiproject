d546631eeeb8ddaff616e7eeee1696b1e0eedd2a
mr-indonesia <secretcode190@gmail.com>
2025-08-28 12:21:35 +0700

    add controller

--- min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
+++ min-quotation-takaful/WebAPI/obj/Debug/net8.0/WebAPI.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+7ff687faa356fe623c42e50c6b77457ee09f1966")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebAPI")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
