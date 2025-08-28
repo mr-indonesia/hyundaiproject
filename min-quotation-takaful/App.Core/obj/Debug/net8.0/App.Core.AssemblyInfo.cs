ea958d16a5f279ae97c412c681443de86944b4d7
mr-indonesia <secretcode190@gmail.com>
2025-08-28 12:04:35 +0700

    add publis

--- min-quotation-takaful/App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
+++ min-quotation-takaful/App.Core/obj/Debug/net8.0/App.Core.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+82481e57214bcc1c5493237fb61867f825ea89af")]
 [assembly: System.Reflection.AssemblyProductAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyTitleAttribute("App.Core")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
